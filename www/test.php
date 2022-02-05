854d5259528542fe96f72b58c1d21fd48733f29c
cgladis <cgladis@student.21-school.ru>
2022-02-05 16:01:20 +0300

    add css to php

--- www/test.php
+++ www/test.php
@@ -1,20 +1,33 @@
-#!/Users/ashorshin/.brew/bin/php-cgi
-
 <?php
 session_start();
-
-if (!isset($_SESSION['count']))
-{
-  $_SESSION['count'] = 1;
-}
-else
-{
-  ++$_SESSION['count'];
-}
-
-echo $_SESSION['count'];
-
 ?>
 
+<html>
+<head>
+    <link rel="stylesheet" href="../css/cgi_main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<div class="content">
+    <h1> You visited this page <span>
+
+    <?php
+    if (!isset($_SESSION['count']))
+    {
+      $_SESSION['count'] = 1;
+    }
+    else
+    {
+      ++$_SESSION['count'];
+    }
 
 
+    echo $_SESSION['count'];
+
+    ?>
+
+    <span> times.<h1>
+</div>
+</body>
+</html>
